2637a0ef47ffcad9e87c5401a7bbf5d72ae61434
Ema <59629187+Amethysm@users.noreply.github.com>
2021-02-20 14:22:56 +0700

    Linked calculation file with discord somewhat (#6)

--- cogs/core.py
+++ cogs/core.py
@@ -6,6 +6,7 @@ import json
 from loguru import logger
 import networkx as nx # Required for node connections
 import matplotlib.pyplot as plt # Required for image processing and saving
+from utils.CalculateLib import *
 
 class Core(commands.Cog):
     def __init__(self, bot):
@@ -16,7 +17,7 @@ class Core(commands.Cog):
     async def info(self, ctx:commands.Context, mode, name, level=None):
         if level is None:
             name = name.lower()
-            with open(f'{str(os.getcwd())}\{mode}\{name}.json','r') as f:
+            with open(f'{str(os.getcwd())}{os.path.sep}{mode}{os.path.sep}{name}.json','r') as f:
                 temp = json.load(f)
             name = name.capitalize()
             embed = Embed(color = 0x00ff00)
@@ -25,14 +26,14 @@ class Core(commands.Cog):
                 for i in temp['generalInfo']:
                     if i == "imageAddress":
                         continue
-                    a += f'**{i}**: {temp["generalInfo"][i]}\n\n'
+                    a += f'**{i}**: {temp["generalInfo"][i]}\n'
                 embed.set_image(url=temp['generalInfo']['imageAddress'])
                 embed.add_field(name = f"General stats of {name.capitalize()}:",value = a)
                 embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
                 return await ctx.send(embed=embed)
             else:
                 name = name.lower()
-                with open(f'{str(os.getcwd())}\{mode}\{name}.json','r') as f:
+                with open(f'{str(os.getcwd())}{os.path.sep}{mode}{os.path.sep}{name}.json','r') as f:
                     for i in temp['generalInfo']:
                         a += f'**{i}**: {temp["generalInfo"][i]}\n\n'
                     embed.set_image(url=temp[str(len(temp)-1)]['imageAddress'])
@@ -43,7 +44,7 @@ class Core(commands.Cog):
         if level is not None:
             if mode == 'node':   
                 name = name.lower()
-                with open(f'{str(os.getcwd())}\{mode}\{name}.json','r') as f:
+                with open(f'{str(os.getcwd())}{os.path.sep}{mode}{os.path.sep}{name}.json','r') as f:
                     temp = json.load(f)
                 name = name.capitalize()
                 embed = discord.Embed(color = 0x00ff00)
@@ -58,7 +59,7 @@ class Core(commands.Cog):
                 return await ctx.send(embed=embed)
             elif mode.lower() == 'program':
                 name = name.lower()
-                with open(f'{str(os.getcwd())}\{mode}\{name}.json','r') as f:
+                with open(f'{str(os.getcwd())}{os.path.sep}{mode}{os.path.sep}{name}.json','r') as f:
                     temp = json.load(f)
                 name = name.capitalize()
                 embed = discord.Embed(color = 0x00ff00)
@@ -72,8 +73,7 @@ class Core(commands.Cog):
                 embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
                 return await ctx.send(embed=embed)
 
-
-    @commands.command()
+    @commands.command(name="netBuild", description="Creates a visual representation of user's network.", usage = "")
     async def netBuild(self, ctx:commands.Context):
         g = nx.Graph() # Initialise a graph
         nodeList = {} # Initialise the node list - it contains info on node connections
@@ -134,12 +134,120 @@ class Core(commands.Cog):
                 g.add_edge(node, connectedNode)
 
         nx.draw(g, with_labels = True)
-        os.remove("image.png")
         plt.savefig("image.png")
-        file = open(f"{os.getcwd()}\image.png",'rb')
-        await ctx.send(file =discord.File(fp=file))
+        file = open(f"{os.getcwd()}{os.path.sep}image.png",'rb')
+        await ctx.send(file=discord.File(fp=file))
         file.close()
-        os.remove(f"{os.getcwd()}\image.png")
+        os.remove(f"{os.getcwd()}{os.path.sep}image.png")
 
+
+    @commands.command()
+    async def calculate(self, ctx:commands.Context):
+        await ctx.send('What node are you using to store the programs?')
+        takenNode = {'defProg':{}}
+        program = {}
+        node = {}
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+        a = await self.bot.wait_for('message', check = check)
+        a = a.content.lower().split()
+        with open(f"{os.getcwd()}{os.path.sep}node{os.path.sep}{a[0]}.json",'r') as f:
+            b = json.loads(f.read())
+            takenNode['firewall'] = int(b[str(a[1])]['NodeFirewall'].replace(',',''))
+            takenNode['fixedFirewall'] = int(b[str(a[1])]['NodeFirewall'].replace(',',''))
+            takenNode['regen'] = int(b['generalInfo']['RegenerationRate'].replace('%',""))
+        await ctx.send('What defensive program are you using?')
+        a = await self.bot.wait_for('message',check=check)
+        a = a.content.lower()
+        c= 0
+        if a == 'none':
+            pass
+        else:
+            a = a.split()
+            c = 0
+            for i in a:
+                c += 1
+                b = i.split('-')
+                with open(f"{os.getcwd()}{os.path.sep}program{os.path.sep}{b[0]}.json",'r') as f:
+                    d = json.loads(f.read())
+                if b[0] == 'protector':
+                    takenNode['defProg'][str(c)] = [int(d[str(b[1])]['BufferSize'].replace(',','')), 7, int(d[str(b[1])]['BufferSize'].replace(',',''))]
+                else:
+                    takenNode['defProg'][str(c)] = [int(d[str(b[1])]['BufferSize'].replace(',','')), 0, int(d[str(b[1])]['BufferSize'].replace(',',''))]
+            c = 0
+        await ctx.send("What program are you using?")
+        a = await self.bot.wait_for('message',check = check)
+        a = a.content.lower().split()
+        for i in a:
+            c += 1
+            b = i.split('-')
+            print(b)
+            with open(f"{os.getcwd()}{os.path.sep}program{os.path.sep}{b[0]}.json",'r') as f:
+                d = json.loads(f.read())
+            program[str(c)] = {}
+            program[str(c)]['damage'] = float(d[str(b[1])]['Strength(DPS)']) * float(d['generalInfo']['ProjectileTime'].replace(" Second",'').replace("s",''))
+            program[str(c)]['installTime'] = float(d['generalInfo']['InstallTime'].replace(" Second",'').replace("s",''))
+            program[str(c)]['interval'] = float(d['generalInfo']['Delay'].replace(" Second",'').replace("s",''))
+            program[str(c)]['projectileTime'] = float(d['generalInfo']['ProjectileTime'].replace(" Second",'').replace("s",''))
+            program[str(c)]['localCounter'] = 0
+            if b[0] == 'shuriken':
+                program[str(c)]['mode'] = 'multi'
+            else:
+                program[str(c)]['mode'] = None
+            if b[0] == 'blaster':
+                program[str(c)]['stun'] = 2
+            else:
+                program[str(c)]['stun'] = 0
+        await ctx.send("What node will be tested?")
+        a = await self.bot.wait_for('message',check = check)
+        a = a.content.lower().split()
+        for i in a:
+            b = i.split('-')
+            with open(f"{os.getcwd()}{os.path.sep}node{os.path.sep}{b[0]}.json",'r') as f:
+                d = json.loads(f.read())
+            node[b[0]] = {}
+            node[b[0]]['guardians'] = {}
+            node[b[0]]['sentryCounter'] = 0
+            node[b[0]]['nodeCounter'] = 0
+            node[b[0]]['stunCounter'] = 0
+            node[b[0]]['firewall'] = int(d[str(b[1])]['NodeFirewall'].replace(',',''))
+            node[b[0]]['fixedFirewall'] = int(d[str(b[1])]['NodeFirewall'].replace(',',''))
+            node[b[0]]['regen'] = int(d['generalInfo']['RegenerationRate'].replace('%',''))
+            if "Strength(DPS)" not in d[str(b[1])]:
+                node[b[0]]['DPS'] = 0
+                node[b[0]]['interval'] = 0
+            else:
+                node[b[0]]['DPS'] = float(d[str(b[1])]['Strength(DPS)']) * float(d['generalInfo']['ProjectileTime'].replace(' Second','').replace('s',''))
+                node[b[0]]['interval'] = float(d['generalInfo']['ProjectileTime'].replace(' Second','').replace('s',''))
+        c = 0
+        await ctx.send("Do they have AV installed? If so, what level is it?")
+        a = await self.bot.wait_for('message',check=check)
+        a = a.content.lower()
+        if a == 'no' or a == 'none':
+            for i in node:
+                node[i]['sentryDPS'] = 0
+        else:
+            with open(f"{os.getcwd()}{os.path.sep}node{os.path.sep}sentry.json",'r') as f:
+                d = json.loads(f.read())
+            for i in node:
+                node[i]['sentryDPS'] = int(d[a]['Strength(DPS)'])
+        await ctx.send("Specify which guardian is connected to which node. 'end' to stop.")
+        while True:
+            a = await self.bot.wait_for('message',check = check)
+            a = a.content
+            if a.lower() == 'end':
+                break
+            a = a.lower().split()
+            b = a[1].split('-')
+            for i in b:
+                c += 1
+                with open(f"{os.getcwd()}{os.path.sep}node{os.path.sep}guardian.json",'r') as f:
+                    d = json.loads(f.read())
+                node[a[0]]['guardians'][str(c)] = [int(d[i]['ShieldBuffer'].replace(',','')), 0, int(d[i]['ShieldBuffer'].replace(',',''))]
+        print(takenNode)
+        print(program)
+        print(node)
+        await ctx.send(bruteCal(takenNode,program,node))
+                                              
 def setup(bot):
     bot.add_cog(Core(bot))
--- utils/CalculateLib.py
+++ utils/CalculateLib.py
@@ -8,12 +8,20 @@ def bruteCal(takenNode, program, node):
     tempDamage = 0
     preGuardianBuffer = 0
     fallenNode = 0
+    deadProg = 0
     while True:
+        for k in node:
+            if node[k]['firewall'] <= 0:
+                node[k]['firewall'] = 0
+                fallenNode += 1
+        if time >= 180:
+            return [time, 'Time//Impossible']
         if takenNode['firewall'] <= 0:
             return [time, "TakenNode//Retaken",takenNode['firewall']]
         if fallenNode == len(node):
             return [time, "TakenNode//Success",takenNode['firewall']]
         fallenNode = 0
+        deadProg = 0
         if time >= 180:
             time = 180
             return [time, "Time//Impossible",takenNode['firewall']]
@@ -21,54 +29,61 @@ def bruteCal(takenNode, program, node):
             tempDamage = program[j]['damage']
             if time >= program[j]['installTime']:
                 program[j]['localCounter'] += 0.1
-            if program[j]['localCounter'] == max(program[j]['interval'] - program[j]['projectileTime'], 0.1):
+                program[j]['localCounter'] = round(program[j]['localCounter'], 2)
+            if program[j]['localCounter'] >= max(program[j]['interval'], 0.1):
                 tempDamage = program[j]['damage']
                 for k in node:
-                    if node[k]['firewall'] <= 0:
-                        node[k]['firewall'] = 0
-                        fallenNode += 1
-                        print(fallenNode)
-                        continue
                     if node[k]['firewall'] > 0:
                         if program[j]['mode'] == 'multi':
                             tempDamage = program[j]['damage']
                         for l in node[k]['guardians']:
-                            if node[k]['guardians'][l][1] == time:
+                            if node[k]['guardians'][l][1] == time and node[k]['guardians'][l][0] <= 0:
                                 node[k]['guardians'][l][0] = node[k]['guardians'][l][2] #reset guardian shield
                             if node[k]['guardians'][l][0] > 0:
                                 node[k]['guardians'][l][1] = time + 7 #reset guardian shield delay
                             if tempDamage <= 0:
                                 continue
+                            if node[k]['guardians'][l][0] <= 0:
+                                continue
                             preGuardianBuffer = node[k]['guardians'][l][0]
-                            node[k]['guardians'][l][0] -= tempDamage
+                            node[k]['guardians'][l][0] = max(node[k]['guardians'][l][0] - tempDamage,0)
                             tempDamage = max(tempDamage - preGuardianBuffer, 0)
                         node[k]['firewall'] -= tempDamage
                     if program[j]['stun'] != 0:
                         node[k]['stunCounter'] = time + program[j]['stun']
-                    if node[k]['stunCounter'] <= time:
-                        node[k]['firewall'] += node[k]['firewall'] / 1000 * node[k]['regen'] 
+                    if node[k]['stunCounter'] <= time and node[k]['firewall'] >= 0:
+                        node[k]['firewall'] += node[k]['firewall'] / 1000 * node[k]['regen']
+                        if node[k]['firewall'] >= node[k]['fixedFirewall']:
+                           node[k]['firewall'] = node[k]['fixedFirewall']
                 program[j]['localCounter'] = 0
+        takenNode['firewall'] += takenNode['firewall'] / 1000 * takenNode['regen']
+        takenNode['firewall'] = round(takenNode['firewall'],1)
+        if takenNode['firewall'] >= takenNode['fixedFirewall']:
+            takenNode['firewall'] = takenNode['fixedFirewall']
         for k in node:
-            if node[k]['nodeCounter'] == max(node[k]['interval'] - node[k]['projectileTime'], 0.1):
+            if node[k]['nodeCounter'] == max(node[k]['interval'], 0.1):
                 for n in takenNode['defProg']:
-                    takenNode['defProg'][n][0] += takenNode['defProg'][n][0] / 100 * takenNode['defProg'][n][1]
-                    takenNode['defProg'][n][0] -= node[k]['DPS']
+                    if takenNode['defProg'][n][0] <= 0:
+                        deadProg += 1
+                        continue
+                    if k != 'sentry':
+                        takenNode['defProg'][n][0] += takenNode['defProg'][n][0] / 1000 * takenNode['defProg'][n][1]
+                        takenNode['defProg'][n][0] -= node[k]['DPS']
                     if node[k]['sentryCounter'] == 1:
                         takenNode['defProg'][n][0] -= node[k]['sentryDPS']
                         node[k]['sentryCounter'] = 0
                     node[k]['nodeCounter'] = 0
-                    if takenNode['defProg'][n][0] <= 0:
-                        takenNode['defProg'].pop(n)
-                if len(takenNode['defProg']) == 0:
-                    takenNode['firewall'] -= node[k]['DPS']
+                if deadProg == len(takenNode['defProg']):
+                    if k != 'sentry':
+                        takenNode['firewall'] -= node[k]['DPS']
                     if node[k]['sentryCounter'] == 1:
                         takenNode['firewall'] -= node[k]['sentryDPS']
                         node[k]['sentryCounter'] = 0
-                node[k]['nodeCounter'] = 0
+                    node[k]['nodeCounter'] = 0
+            node[k]['sentryCounter'] = round(node[k]['sentryCounter']+0.1,1)
+            node[k]['nodeCounter'] = round(node[k]['nodeCounter']+0.1,1)
         time += 0.1
-        print(takenNode['firewall'], end= f"       {node['1']['firewall']}\n")
-        #if time >= 180:
-        #    return [time, 'Time//Impossible', takenNode['firewall'], node[0]['firewall']]
+        time = round(time,2)
         
 if __name__ == '__main__':
     takenNode = json.loads(input())
